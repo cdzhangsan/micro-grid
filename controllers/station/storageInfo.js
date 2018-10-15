2521ce38fb4a3c9165c056bf215d1d25fa70e9ef
cdzhangsan <25335723@qq.com>
2018-10-15 09:45:49 +0800

    mod

--- controllers/station/storageInfo.js
+++ controllers/station/storageInfo.js
@@ -1,3 +1,8 @@
+var tools =require('../../utils/util');
+var nt = tools.getTime().time;
+var nD= tools.getTime().date;
+
+
 var SqliteDB = require('../../utils/sqlite').SqliteDB;
 var file = "/root/mbiot-master/cloud_data.db";
 var sqliteDB = new SqliteDB(file);
@@ -13,7 +18,7 @@ module.exports=function(req, res){
             // ,"s_dischargePower":523
             // ,"s_soc":56
             "s_soh":98
-            ,"dayTotalcharge":100
+            ,"dayTotalcharge":(1.2/24*nt).toFixed(2)
             ,"totalCostSaving":0
             ,"dayRealTimePower":[133,122,133,111,133,111,145,164,112,143,133,122,133,111,133,111,145,164]
             ,"monthdisCharge":[1.1,1.2,1,1,1,1,1,0.9,0.9,0.9,1.1,1.2,1,1,1,1,1,0.9,0.9,0.9,1.1,1.2,1,1,1,1,1,0.9,0.9,0.9]
@@ -22,11 +27,11 @@ module.exports=function(req, res){
         var _d2 = objects2[0].dev_data;
         var obj2 = JSON.parse(_d2);
         nv.test2 = obj2;
-        nv.data.s_realTimePower = (obj2["负载功率"]/1000).toFixed(3);
+        nv.data.s_realTimePower = (obj2["取电功率"]/1000).toFixed(3);
         nv.data.s_chargePower =(5*(1-obj2["电池SOC"]/100)).toFixed(2);
         nv.data.s_dischargePower =(5*obj2["电池SOC"]/100).toFixed(2);
         nv.data.s_soc = obj2["电池SOC"];
         res.json(nv)
     })
-    };
+};
 
--- controllers/station/topologicalInfo.js
+++ controllers/station/topologicalInfo.js
@@ -1,12 +1,19 @@
+var tools =require('../../utils/util');
+
+var SqliteDB = require('../../utils/sqlite').SqliteDB;
+var file = "/root/mbiot-master/cloud_data.db";
+var sqliteDB = new SqliteDB(file);
+var querySql = 'select * from cloud_data where device_no="lab-211" order by id desc limit 0,1';
+var querySql2 = 'select * from cloud_data where device_no="lab-111" order by id desc limit 0,1';
 
 module.exports=function(req, res){
         var nv={};
         nv.code = '1';
         nv.data = {
             "runType":1
-            ,"soc":56
-            ,"storageVoltage":0
-            ,"storageCurrent":1
+            // ,"soc":56
+            // ,"storageVoltage":0
+            // ,"storageCurrent":1
             ,"unitMaxTemperature":28
             ,"unitMinTemperature":350
             ,"unitMaxVoltage":350
@@ -15,6 +22,19 @@ module.exports=function(req, res){
             ,"pileStandby":0
             ,"pileFault":0
         };
+    sqliteDB.queryData(querySql2, function(objects2){
+        var _d2 = objects2[0].dev_data;
+        var obj2 = JSON.parse(_d2);
+        nv.test2 = obj2;
+        nv.data.storageVoltage = obj2["电池堆总电压"];
+        nv.data.storageCurrent = obj2["电池堆总电流"];
+        nv.data.unitMaxTemperature = obj2["电池堆最高温度"];
+        nv.data.unitMinTemperature = obj2["电池堆最低温度"];
+        nv.data.unitMaxVoltage = obj2["电池堆最高电芯电压"].toFixed(2);
+        nv.data.unitMinVoltage = obj2["电池堆最低电芯电压"].toFixed(2);
+        // nv.data.s_chargePower =(5*(1-obj2["电池SOC"]/100)).toFixed(2);
+        nv.data.soc = obj2["电池SOC"];
         res.json(nv)
+    })
     };
 
